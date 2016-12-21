7b1781af3cdcdb0bbc33495a7dc142ea9601b28b
JasonVann <jasonvanet@gmail.com>
2016-12-20 23:12:53 -0800

    Added implementation for range and type

--- SoftwareDebugging/Bonsaikon.py
+++ SoftwareDebugging/Bonsaikon.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 # Simple Daikon-style invariant checker
 # Andreas Zeller, May 2012
-# Complete the provided code around lines 28 and 44
-# Do not modify the __repr__ functions.
+# Complete the provided code, using your code from
+# first exercise and adding ability to infer assertions
+# for variable type, set and relations
 # Modify only the classes Range and Invariants,
 # if you need additional functions, make sure
 # they are inside the classes.
@@ -20,22 +21,29 @@ def square_root(x, eps = 0.00001):
 def square(x):
     return x * x
 
+def double(x):
+    return abs(20 * x) + 10
+
 # The Range class tracks the types and value ranges for a single variable.
 class Range:
     def __init__(self):
         self.min  = None  # Minimum value seen
         self.max  = None  # Maximum value seen
+        self.type = None  # Type of variable
+        self.set = set()  # Set of values taken
     
     # Invoke this for every value
     def track(self, value):
-        if self.max == None or value > self.max:
+        # YOUR CODE
+        if self.max is None or value > self.max:
             self.max = value
-        if self.min == None or value < self.min:
+        if self.min is None or value < self.min:
             self.min = value
+        self.type = type(value)
+        self.set.add(value)
             
     def __repr__(self):
-        return repr(self.min) + ".." + repr(self.max)
-
+        repr(self.type) + " " + repr(self.min) + ".." + repr(self.max)+ " " + repr(self.set)
 
 # The Invariants class tracks all Ranges for all variables seen.
 class Invariants:
@@ -49,49 +57,58 @@ class Invariants:
         if event == "call" or event == "return":
             # YOUR CODE HERE. 
             # MAKE SURE TO TRACK ALL VARIABLES AND THEIR VALUES
-            # If the event is "return", the return value
-            # is kept in the 'arg' argument to this function.
-            # Use it to keep track of variable "ret" (return)
             fn = frame.f_code.co_name
-            param = frame.f_locals.keys()[0]
+            #param = frame.f_locals.keys()[0]
             if fn not in self.vars:
                 dic = {}
                 self.vars[fn] = dic
+                
             if event not in self.vars[fn]:
                 dic = self.vars[fn]
                 dic2 = {}
-                if event == 'return':
-                    dic2['ret'] = Range()
-                else:
-                    dic2[param] = Range()
+                dic2['ret'] = Range()
+                dic2['ret'].track(arg)
                 dic[event] = dic2
-            else:
-                print 63, self.vars, fn, param, '\n'
-                print 64, frame, event, arg, '\n'
-                print 67, frame.f_locals, '\n'
-                if event == 'return':
-                    self.vars[fn][event]['ret'].track(arg)
-                else:
-                    v = frame.f_locals[param]
-                    self.vars[fn][event][param].track(v)
-                print 76, self.vars
-
+               
+            self.vars[fn][event]['ret'].track(arg)
+            for param in frame.f_locals:
+                if param not in self.vars[fn][event]:
+                    self.vars[fn][event][param] = Range()
+                v = frame.f_locals[param]
+            
+                self.vars[fn][event][param].track(v)
+                    
     def __repr__(self):
         # Return the tracked invariants
         s = ""
         for function, events in self.vars.iteritems():
             for event, vars in events.iteritems():
                 s += event + " " + function + ":\n"
-                # continue
-                
+        
                 for var, range in vars.iteritems():
+                    #print 89, range.min
+                    s += "    assert isinstance(" + var + ", type(" + repr(range.min) + "))" + "\n"
+                    s += "    assert " + var + " in " + repr(range.set) + "\n"
                     s += "    assert "
                     if range.min == range.max:
                         s += var + " == " + repr(range.min)
                     else:
                         s += repr(range.min) + " <= " + var + " <= " + repr(range.max)
                     s += "\n"
-                
+                    # ADD HERE RELATIONS BETWEEN VARIABLES
+                    # RELATIONS SHOULD BE ONE OF: ==, <=, >=
+                    #s += "    assert " + var + " >= " + var2 + "\n" 
+                    for var2, range2 in vars.iteritems():
+                        #print 125, var2, var
+                        if var2 is not var:
+                            if (range.min == range2.min) and (range.max == range2.max):
+                                op = "=="
+                            if range.min <= range2.min:
+                                op = "<="
+                            else:
+                                op = ">="
+                            s += "    assert " + var + " " + op + " " + var2 + "\n"
+                    #print 126, s
         return s
 
 invariants = Invariants()
@@ -103,16 +120,20 @@ def traceit(frame, event, arg):
 sys.settrace(traceit)
 # Tester. Increase the range for more precise results when running locally
 eps = 0.000001
-
-inputs = [3, 0, 10]
-
-for i in range(0, 3):
-    #r = int(random.random() * 1000) # An integer value between 0 and 999.99
-    r = inputs[i]
-    z = square_root(r, eps)
-    z = square(z)
-
+test_vars = [34.6363, 9.348, -293438.402]
+for i in test_vars:
+#for i in range(1, 10):
+    z = double(i)
 sys.settrace(None)
 print invariants
 
+# Example sample of a correct output:
+"""
+return double:
+    assert isinstance(x, type(-293438.402))
+    assert x in set([9.348, -293438.402, 34.6363])
+    assert -293438.402 <= x <= 34.6363
+    
+    assert x <= ret
+"""
 
