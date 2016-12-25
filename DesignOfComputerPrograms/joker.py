7a010ce39c98744d084520751d8aa97d6fbacc83
JasonVann <jasonvanet@gmail.com>
2016-12-25 14:48:27 -0800

    Added implementation of best_hand

--- DesignOfComputerPrograms/joker.py
+++ DesignOfComputerPrograms/joker.py
@@ -1,12 +1,20 @@
-# CS 212, hw1-1: 7-card stud
+# CS 212, hw1-2: Jokers Wild
 #
 # -----------------
 # User Instructions
 #
-# Write a function best_hand(hand) that takes a seven
-# card hand as input and returns the best possible 5
-# card hand. The itertools library has some functions
-# that may help you solve this problem.
+# Write a function best_wild_hand(hand) that takes as
+# input a 7-card hand and returns the best 5 card hand.
+# In this problem, it is possible for a hand to include
+# jokers. Jokers will be treated as 'wild cards' which
+# can take any rank or suit of the same color. The 
+# black joker, '?B', can be used as any spade or club
+# and the red joker, '?R', can be used as any heart 
+# or diamond.
+#
+# The itertools library may be helpful. Feel free to 
+# define multiple functions if it helps you solve the
+# problem. 
 #
 # -----------------
 # Grading Notes
@@ -18,11 +26,20 @@
 
 import itertools
 
-def best_hand(hand):
-    "From a 7-card hand, return the best 5 card hand."
+def best_wild_hand(hand):
+    "Try all values for jokers in all 5-card selections."
     
     # Your code here
-    
+
+def test_best_wild_hand():
+    assert (sorted(best_wild_hand("6C 7C 8C 9C TC 5C ?B".split()))
+            == ['7C', '8C', '9C', 'JC', 'TC'])
+    assert (sorted(best_wild_hand("TD TC 5H 5C 7C ?R ?B".split()))
+            == ['7C', 'TC', 'TD', 'TH', 'TS'])
+    assert (sorted(best_wild_hand("JD TC TH 7C 7D 7S 7H".split()))
+            == ['7C', '7D', '7H', '7S', 'JD'])
+    return 'test_best_wild_hand passes'
+
 # ------------------
 # Provided Functions
 # 
@@ -85,15 +102,4 @@ def two_pair(ranks):
         return (pair, lowpair)
     else:
         return None 
-    
-def test_best_hand():
-    assert (sorted(best_hand("6C 7C 8C 9C TC 5C JS".split()))
-            == ['6C', '7C', '8C', '9C', 'TC'])
-    assert (sorted(best_hand("TD TC TH 7C 7D 8C 8S".split()))
-            == ['8C', '8S', 'TC', 'TD', 'TH'])
-    assert (sorted(best_hand("JD TC TH 7C 7D 7S 7H".split()))
-            == ['7C', '7D', '7H', '7S', 'JD'])
-    return 'test_best_hand passes'
-
-print test_best_hand()
 
