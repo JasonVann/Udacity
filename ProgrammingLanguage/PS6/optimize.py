c460328c7c9af22459a4e12d154a58a5f3ba2459
JasonVann <jasonvanet@gmail.com>
2017-01-04 11:15:04 -0800

    Added optimization

--- ProgrammingLanguage/PS6/optimize.py
+++ ProgrammingLanguage/PS6/optimize.py
@@ -29,20 +29,44 @@
 def optimize(exp): 
     etype = exp[0] 
     if etype == "binop":
+        #print 32, exp
         a = optimize(exp[1])
         op = exp[2]
         b = optimize(exp[3])
 
+        #print 36, a, b, op
+        #print 38, op == '+', b == ('number', '0.0')
+        #print 38, op == '-', a[1] == b[1]
         # Try Arithmetic Laws
         if op == "*" and (a == ("number",0) or b == ("number",0)):
             return ("number",0) 
         # Fill in more optimizations here ...
-
+        elif op == "*" and b == ("number", 1):
+            return a
+        elif op == "+" and b == ("number", 0):
+            return a
+        elif op == "*" and a == ("number", 1):
+            return b
+        elif op == "+" and a == ("number", 0):
+            return b
+        elif op == '-' and a[1] == b[1]:
+            #print 52, a, b, op
+            return ('number', 0)
         # Try Constant Folding
         # Fill in more optimizations here ...
-
+        elif a[0] == 'number' == b[0]:
+            if op == '*':
+                return ('number', a[1]*b[1])
+            elif op == '/':
+                return ('number', a[1]/b[1])
+            elif op == '+':
+                return ('number', a[1] + b[1])
+            elif op == '-':
+                return ('number', a[1] - b[1])
         # If all else fails, return something good here ... 
-
+        
+        #print 68, a, b, op
+        return (etype, a, op, b)
     # leave this expression un-optimized 
     return exp 
 
@@ -62,20 +86,22 @@ def times(a,b):
         return ("binop",a,"*",b) 
 
 exp1 = times(two,zero) 
-print optimize(exp1) == zero 
+#print optimize(exp1) == zero 
 
 exp2 = times(darius,minus(two,two))
-print optimize(exp2) == zero 
+#print optimize(exp2) == zero 
 
 exp3 = minus(plus(zero,plus(one,plus(two,zero))),two)
-print optimize(exp3) == one
+#print optimize(exp3) == one
 
+#print optimize(plus(minus(musa,musa),plus(musa,zero)))
 five = plus(two,plus(two,one))
+#print optimize(five)
 exp4 = times(five,(plus(minus(musa,musa),plus(musa,zero))))
 print optimize(exp4) == ('binop', ('number', 5.0), '*', ('var', 'musa'))
 
 big_exp = zero 
 for i in range(10):
         big_exp = ("binop",big_exp,"+",("number",i))
-print optimize(big_exp) == ("number", 45.0) # 0+1+2+3+4+5+6+7+8+9
+#print optimize(big_exp) == ("number", 45.0) # 0+1+2+3+4+5+6+7+8+9
 
